be04d538cdfa7a1f8fb74e92a5d9622ded105895
haha1ship <3522829518@qq.com>
2026-05-21 09:21:13 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,19 @@
+window.addEventListener("wheel",(e)=>{
 
+    if(e.deltaY > 0){
+
+        window.scrollTo({
+            top:window.innerHeight,
+            behavior:"smooth"
+        });
+
+    }else{
+
+        window.scrollTo({
+            top:0,
+            behavior:"smooth"
+        });
+
+    }
+
+});
